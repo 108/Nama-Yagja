db3d269b1dbfa7226d5bf9e466f113cd68115e47
108media <info@108media.hu>
2011-07-16 13:15:13 +0200

    Seo frendly url

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -21,5 +21,33 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
             // Return it, so that it can be stored by the bootstrap
             return $view;
         }
+        
+     public function _initRoutes()
+        {
+
+            $frontController = Zend_Controller_Front::getInstance();
+            $router = $frontController->getRouter();
+
+            $router->addRoute(
+                    'testimonialsOnIndex', 
+                    new Zend_Controller_Router_Route('/testimonials' , 
+                            array('controller' => 'index',
+                                  'action'     => 'testimonials'))
+                    );
+            
+            $router->addRoute(
+                    'registrationOnIndex', 
+                    new Zend_Controller_Router_Route('/registration' , 
+                            array('controller' => 'index',
+                                  'action'     => 'registration'))
+                    );
+            
+            $router->addRoute(
+                    'contactusOnIndex', 
+                    new Zend_Controller_Router_Route('/contactus' , 
+                            array('controller' => 'index',
+                                  'action'     => 'contactus'))
+                    );
+        }
 }
 
